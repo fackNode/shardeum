4c196d3bd46357bb77a0ee9f1ca915cc14ea0a7e
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2023-02-15 21:35:33 +0300

    Create HMEXT_ports_checker.sh

---
+++ HMEXT_ports_checker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+check_port() {
+  if nc -z localhost $1; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+port=8080
+
+while true; do
+  if check_port $port; then
+    port=$(shuf -i 1025-65536 -n 1)
+  else
+    break
+  fi
+done
+
+USEPORT=$port
+
+export USEPORT
